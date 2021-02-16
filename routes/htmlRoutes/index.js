ac4230c3f6d832d2f494499488c67bcfccdfd2a3
Nate Schroeder <n8dogg59@hotmail.com>
2021-02-15 20:53:16 -0600

    code snapshot

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -13,4 +13,12 @@ router.get('/zookeepers', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
 });
 
+router.get('/aquarium', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/aquarium.html'));
+});
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
 module.exports = router;
